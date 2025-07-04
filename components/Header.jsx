54d77d6cad91314163debba1f045d2246c45f5b3
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2025-07-04 23:10:20 +0545

    feat: header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,34 @@
-import ThemeToggler from "./ThemeToggler"
+"use client"
+import React from "react";
+import Image from "next/image";
 
-
+import Logo from "./Logo"; // Adjust the path as necessary
+import ThemeToggler from "./ThemeToggler";
+import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 function Header() {
   return (
-    <header><ThemeToggler/></header>
-  )
+    <header>
+      <div className="container mx-auto">
+        <div className="flex items-center justify-between py-4">
+          <Logo />
+          <div>
+          <Nav />
+          <ThemeToggler />
+
+
+          {/*mobile nev*/} 
+          <div>
+            <MobileNav />
+          </div>
+
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+
+export default Header;
